d19f9a461a877180e67f848e45cd0a572dc8a0af
franciscogallardo <fragalcer@gmail.com>
2017-08-15 23:43:52 +1000

    Initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello World " + name);
+            
+           Random random = new Random();
+            Console.WriteLine(random.Next(1,11));
+        }
+    }
+}
